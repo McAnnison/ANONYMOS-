f42d7b020c20b9fc23973cbb09c6ccada142909a
Mensah Kwame Anni <mkanni@st.ug.edu.gh>
2024-06-30 22:41:37 +0000

    Update DREAM.py
    Getting started with a guess game.

--- DREAM.py
+++ DREAM.py
@@ -1,1 +1,25 @@
+import random
 
+def number_guess_game():
+    print("Welcome to MathMagnet")
+    print("Are you feeling bored!")
+    print("Come with me.....")
+    print("I'm thinking of a number between 1 and 100.")
+
+    hidden_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("Enter your guess: "))
+
+        if guess < hidden_number:
+            print("Your number is too low, try again.")
+        elif guess > hidden_number:
+            print("Tour number is too high, try again.")
+        else:
+            print(f"Congratulations! You've guessed the number {hidden_number} correctly in {attempts} attempts.")
+            break
+
+        attempts += 1
+
+number_guess_game()
